c1d1b494b523ec5bae7be3be03fb3210d6160bee
ariyaninj otgongerel <ariyaninj@gmail.com>
2021-05-08 20:36:30 +0800

    first

---
+++ src/sample/Controller.java
@@ -0,0 +1,48 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    @FXML
+    private Button playbtn;
+
+    @FXML
+    private Button profilebtn;
+
+    @FXML
+    private Button leaderboardbtn;
+
+    @FXML
+    private Button aboutbtn;
+
+    @FXML
+    private Label profileName;
+
+    @FXML
+    private Label profileId;
+
+    @FXML
+    void about(MouseEvent event) {
+
+    }
+
+    @FXML
+    void leaderboard(MouseEvent event) {
+
+    }
+
+    @FXML
+    void play(MouseEvent event) {
+
+    }
+
+    @FXML
+    void profile(MouseEvent event) {
+
+    }
+
+}
+
