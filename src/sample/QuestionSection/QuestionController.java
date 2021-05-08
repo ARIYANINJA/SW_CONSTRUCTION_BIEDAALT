524a769eeadf9021cb86b2c379b9d747113ab06e
covidslayer69 <xacap46@gmail.com>
2021-05-09 01:53:36 +0800

    4

--- src/sample/QuestionSection/QuestionController.java
+++ src/sample/QuestionSection/QuestionController.java
@@ -1,4 +1,57 @@
 package sample.QuestionSection;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class QuestionController {
+
+    @FXML
+    private AnchorPane AnchorShde;
+
+    @FXML
+    private TextField txtField;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Button btnChoice1;
+
+    @FXML
+    private Button btnChoice2;
+
+    @FXML
+    private Button btnChoice3;
+
+    @FXML
+    private Button btnChoice4;
+
+    @FXML
+    void Choice1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Choice2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Choice3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Choice4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Exit(ActionEvent event) {
+
+    }
+
 }
