0b182010453f8757cf0708918d49b4555a201462
ariyaninj otgongerel <ariyaninj@gmail.com>
2021-05-09 16:45:52 +0800

    sixth

---
+++ src/add_profile/Add_profile_controller.java
@@ -0,0 +1,21 @@
+package add_profile;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Add_profile_controller {
+
+    @FXML
+    private TextField addProfileText;
+
+    @FXML
+    private Button Addbtn;
+
+    @FXML
+    void Add(MouseEvent event) {
+
+    }
+
+}
---
+++ src/leaderboard/Leaderboard_controller.java
@@ -0,0 +1,29 @@
+package leaderboard;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class Leaderboard_controller {
+
+    @FXML
+    private Label leaderboard1;
+
+    @FXML
+    private Label leaderboard2;
+
+    @FXML
+    private Label leaderboard3;
+
+    @FXML
+    private Label leaderboard4;
+
+    @FXML
+    private Button MainMenubtn;
+
+    @FXML
+    void MainMenu(MouseEvent event) {
+
+    }
+
+}
